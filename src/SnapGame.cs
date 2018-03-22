798100ee6fc9f0420ce9eafb3ae45c7474bf7a3a
morris1500m <6518346@student.swin.edu.au>
2018-03-22 11:36:49 +1100

    changed text color to green

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -58,9 +58,9 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.Red, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.Red, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.Red, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.Green, 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.Green, 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.Green, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 			}
 			else
